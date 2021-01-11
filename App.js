52c8a8a70537022492aa6e53b6628e5a3fb3526d
Obichukwu Onyeowuzoni <Obichukwu@users.noreply.github.com>
2021-01-11 15:54:37 +0000

    Add Sample Navigation

--- App.js
+++ App.js
@@ -1,13 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeFeedScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function HomeSettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+
+
+function HomeScreen() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeFeed" component={HomeFeedScreen} />
+      <Tab.Screen name="HomeSettings" component={HomeSettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function DetailScreen({ navigation }) {
+  const [state, setState] = useState(0)
+  return (
+    <View style={styles.container}>
+      <Text>Open up Again to start working on your app!</Text>
+      <Button title="Go to Details (push)" onPress={() => {
+        navigation.push("Details")
+      }}></Button>
+      <Button title="Go to Details (navgate)" onPress={() => {
+        navigation.navigate("Details")
+      }}></Button>
+      <Button
+        title="Update the title"
+        onPress={() => {
+          setState(st => st + 1)
+          navigation.setOptions({
+            title: 'Custom Header' + state,
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          })
+        }} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Stack.Screen name="Details" component={DetailScreen} options={{ title: 'Overview' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
